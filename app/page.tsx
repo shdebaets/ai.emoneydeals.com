780f2531468f82a1b73904e47140eb1e5279e593
shdebaets <samdebaets04@gmail.com>
2025-11-26 12:51:57 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -89,7 +89,7 @@ export default function Checkout() {
           <div className="mt-3 rounded-xl border border-white/10 bg-black/50 p-3">
             <WhopCheckoutEmbed
               ref={ref}
-              planId="plan_6KwUKw1ckTGZQ"
+              planId="plan_Ruq2C4mj6PZXf"
               theme="dark"
               fallback={<>loading…</>}
               hidePrice={true}  // 👈 hides the price
